09cb384269340f9ae155958e0a8f75475a9f898c
Tim Brown <tpbrown@gmail.com>
2013-04-05 16:04:24 -0700

    Support SSH/SCP with keys between vagrant boxes

--- deploy_to_db.sh
+++ deploy_to_db.sh
@@ -8,6 +8,22 @@ function ec2_ssh {
   ssh -o StrictHostKeyChecking=no -i /var/go/infra_lab.pem ubuntu@$1 $2
 }
 
-ec2_scp puppet.tgz db.part2.com
-ec2_ssh db.part2.com "tar zxvf puppet.tgz"
-ec2_ssh db.part2.com "sudo puppet apply --modulepath=modules db.pp"
+function vagrant_scp {
+	[ ! -f ~/.ssh/vagrant ] && wget https://raw.github.com/mitchellh/vagrant/master/keys/vagrant;chmod 600 ~/.ssh/vagrant
+	scp -o StrictHostKeyChecking=no -i ~/.ssh/vagrant $1 vagrant@$2:
+}
+
+function vagrant_ssh {
+	[ ! -f ~/.ssh/vagrant ] && wget https://raw.github.com/mitchellh/vagrant/master/keys/vagrant;chmod 600 ~/.ssh/vagrant
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/vagrant vagrant@$1 $2
+}
+
+# # EC2
+# ec2_scp puppet.tgz db.part2.com
+# ec2_ssh db.part2.com "tar zxvf puppet.tgz"
+# ec2_ssh db.part2.com "sudo puppet apply --modulepath=modules db.pp"
+
+# Vagrant
+vagrant_scp puppet.tgz db
+vagrant_ssh db "tar zxvf puppet.tgz"
+vagrant_ssh db "sudo puppet apply --modulepath=modules db.pp"
--- deploy_to_web.sh
+++ deploy_to_web.sh
@@ -8,8 +8,26 @@ function ec2_ssh {
   ssh -o StrictHostKeyChecking=no -i /var/go/infra_lab.pem ubuntu@$1 $2
 }
 
-ec2_scp puppet.tgz web.part2.com
-ec2_scp opencart.deb web.part2.com
+function vagrant_scp {
+	[ ! -f ~/.ssh/vagrant ] && wget https://raw.github.com/mitchellh/vagrant/master/keys/vagrant;chmod 600 ~/.ssh/vagrant
+	scp -o StrictHostKeyChecking=no -i ~/.ssh/vagrant $1 vagrant@$2:
+}
 
-ec2_ssh web.part2.com "tar zxvf puppet.tgz"
-ec2_ssh web.part2.com "sudo FACTER_database_host=$DB_HOST FACTER_database_password=$DB_PASSWORD puppet apply --modulepath=modules web.pp"
+function vagrant_ssh {
+	[ ! -f ~/.ssh/vagrant ] && wget https://raw.github.com/mitchellh/vagrant/master/keys/vagrant;chmod 600 ~/.ssh/vagrant
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/vagrant vagrant@$1 $2
+}
+
+# For EC2
+# ec2_scp puppet.tgz webhost.name.amazon.com
+# ec2_scp opencart.deb webhost.name.amazon.com
+
+# ec2_ssh web.part2.com "tar zxvf puppet.tgz"
+# ec2_ssh web.part2.com "sudo FACTER_database_host=$DB_HOST FACTER_database_password=$DB_PASSWORD puppet apply --modulepath=modules web.pp"
+
+# For Vagrant
+vagrant_scp puppet.tgz web
+vagrant_scp opencart.deb web
+
+vagrant_ssh web "tar zxvf puppet.tgz"
+vagrant_ssh web "sudo FACTER_database_host=$DB_HOST FACTER_database_password=$DB_PASSWORD puppet apply --modulepath=modules web.pp"
